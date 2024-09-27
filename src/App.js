9a8ea74db75d536dcf5dcd5ce0318fb886e222c0
NASSAR <radhwen.nasser@capgemini.com>
2024-09-27 09:07:41 +0100

    v02

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import React, { useState } from 'react';
 import PartsCondition from './PartsCondition';
 import './App.css'; // Import your CSS file
 import Home from './Home';
+import {Amplify} from 'aws-amplify';
+import { Authenticator, withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+
+
+
+
+Amplify.configure(awsExports);
 
 const App = () => {
     const [currentPage, setCurrentPage] = useState('home');
@@ -12,6 +21,8 @@ const App = () => {
     };
 
     return (
+        <Authenticator>
+            {({ signOut }) => (
         <div className="app-container">
             <div className="navigation">
                 <button onClick={() => handlePageChange('home')}>Home</button>
@@ -21,7 +32,20 @@ const App = () => {
                 {currentPage === 'home' && <Home />}
                 {currentPage === 'parts-condition' && <PartsCondition />}
             </div>
+            <button 
+                onClick={signOut} 
+                style={{ 
+                  margin: '20px', 
+                  fontSize: '0.8rem', 
+                  padding: '5px 10px', 
+                  marginTop: '20px'
+                }}
+              >
+                Sign Out
+              </button>
         </div>
+        )}
+        </Authenticator>
     );
 };
 
